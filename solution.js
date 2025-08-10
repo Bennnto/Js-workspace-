69b0226028952d7c1e349e00ecd5c989c2d6f7fc
Ben Promkaew <ben.promkaew@icloud.com>
2025-08-10 18:07:11 -0400

    Update solution.js

--- solution.js
+++ solution.js
@@ -9,15 +9,47 @@ function getRandomNumber() {
   }, 500);
 }
 /*====================================================================================*/
-function getNationality(name) {
-  fetch(`https://api.nationalize.io/?name=${name}`)
-      .then(response => response.json())
-      .then((data) => {console.log(`Nationality: ${name}`,data)})
-      .catch(error => {console.error(`fetch API error`, error)})
+async function getNationality(name) {
+  const apis = `https://api.nationalize.io/?name=${name}`
+  try {
+    const response = await fetch(apis);
+    if (!response.ok) {
+      throw new Error (`response status = ${response.status}`);
+    }
+    const data = await response.json()
+    if (!data.country || data.country.length === 0) {
+      return null;
+    }
+    return data;
+  }
+  catch (error) 
+  {
+    console.error("Fetch APIs Error", error.message);  
+    throw error;
+    }
+  }
+/*====================================================================================*/
+async function fetchProducts(id) { 
+  const api = `https://dummyjson.com/products/${id}`
+  try {
+    const response = await fetch(api);
+    if (!response.ok) {
+      throw new Error(`HTTP Error: = ${response.status}`);
+    }
+    const prod = await response.json()
+    return prod.title
+  }
+  catch (error)
+  {
+    return `Could not get proguce: Error: ${error.message}`
+  }
 }
+  
 
 /* ======  Export your functions ===================================================== */
 export {
-  getRandonNumber, getNationality
+  getRandomNumber, 
+  getNationality, 
+  fetchProducts
   // add your other functions, comma separated
 }
